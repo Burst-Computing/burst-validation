040d41119103386e2685272e231e690d31b577fc
Aitor Arjona <aitor.a98@gmail.com>
2023-10-03 12:20:03 +0200

    Remove subtree

--- burst-communication-middleware/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod middleware;
-mod types;
-mod utils;
-
-pub use middleware::*;
-pub use types::*;
-pub use utils::*;
--- burst-communication-middleware/src/types.rs
+++
@@ -1,9 +0,0 @@
-use bytes::Bytes;
-
-#[derive(Debug)]
-pub struct Message {
-    pub sender_id: u32,
-    pub chunk_id: u32,
-    pub last_chunk: bool,
-    pub data: Bytes,
-}
