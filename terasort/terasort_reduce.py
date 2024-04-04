a3aa0586867a20c0c97cdf32a03db2dd30f0a0b3
Enrique Molina <enrique.molina@urv.cat>
2024-04-04 17:16:37 +0200

    separate map and reduce for eks test

---
+++ terasort/terasort_reduce.py
@@ -0,0 +1,53 @@
+import argparse
+
+import pandas as pd
+
+from ow_client.parser import add_openwhisk_to_parser, try_or_except
+from ow_client.time_helper import get_millis
+from ow_client.openwhisk_executor import OpenwhiskExecutor
+from terasort_utils import generate_payload, complete_mpu, add_terasort_to_parser
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    add_openwhisk_to_parser(parser)
+    add_terasort_to_parser(parser)
+    args = try_or_except(parser)
+
+    params = generate_payload(endpoint=args.ts_endpoint, partitions=args.partitions, bucket=args.bucket, key=args.key,
+                              sort_column=0)
+
+    executor = OpenwhiskExecutor(args.ow_host, args.ow_port, args.debug)
+
+    # create pandas dataframe
+    stats = pd.DataFrame(columns=["fn_id",
+                                  "host_submit_reduce",
+                                  "init_fn_reduce",
+                                  "post_download_reduce",
+                                  "pre_upload_reduce",
+                                  "end_fn_reduce",
+                                  "finished"])
+
+    host_submit_reduce = get_millis()
+    dt_reduce = executor.map("terasort-reduce", params,
+                             file="terasort/terasort-reduce.zip",
+                             memory=args.runtime_memory if args.runtime_memory else 256,
+                             custom_image=args.custom_image, is_zip=True)
+    end_time = get_millis()
+    stats = pd.concat([stats,
+                       pd.DataFrame({"fn_id": i["partition_idx"],
+                                     "host_submit_reduce": host_submit_reduce,
+                                     "init_fn_reduce": i["init_fn_reduce"],
+                                     "post_download_reduce": i["post_download_reduce"],
+                                     "pre_upload_reduce": i["pre_upload_reduce"],
+                                     "end_fn_reduce": i["end_fn_reduce"],
+                                     "finished": end_time
+                                     } for i in dt_reduce.get_results())
+                       ])
+
+    stats.to_csv("terasort-classic-onlyreduce.csv", index=False)
+
+    # complete_mpu(endpoint=args.endpoint, bucket=args.bucket, key=params[0]['mpu_key'], upload_id=params[0]["mpu_id"],
+    #              mpu={"Parts": [{"ETag": i['etag'], "PartNumber": i['part_number'] + 1} for i in
+    #                            dt_reduce.get_results().sort(key=lambda x: x['part_number'])]})
+    print("**** Terasort-classic (only-reduce) finished ****")
