bcb983593ad0c0d4a724c076e58d7cc5a35d2686
Enrique Molina <enrique.molina@urv.cat>
2024-04-22 14:46:44 +0200

    adding hypertuning

---
+++ hypertuning/hypertuning/src/lib.rs
@@ -0,0 +1,158 @@
+use aws_config::Region;
+use aws_config::retry::RetryConfig;
+use aws_credential_types::Credentials;
+use aws_sdk_s3::Client as S3Client;
+use burst_communication_middleware::Middleware;
+use bytes::{Bytes};
+use serde_derive::{Deserialize, Serialize};
+use serde_json::{Error, Value};
+use std::fs::File;
+use std::io::Write;
+use std::time::{SystemTime, SystemTimeError, UNIX_EPOCH};
+use tokio::io::AsyncReadExt;
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+struct Input {
+    bucket: String,
+    key: String,
+    s3_config: S3Config,
+    start_byte: i64,
+    end_byte: i64,
+    base_worker_id: u32,
+    granularity: u32,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+struct S3Config {
+    region: String,
+    endpoint: Option<String>,
+    aws_access_key_id: String,
+    aws_secret_access_key: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+struct Output {
+    success: bool,
+    init_fn: String,
+    post_download_chunk: String,
+    input_gathered: String,
+    end_fn: String,
+}
+
+fn get_timestamp_in_milliseconds() -> Result<u128, SystemTimeError> {
+    let current_system_time = SystemTime::now();
+    let duration_since_epoch = current_system_time.duration_since(UNIX_EPOCH)?;
+    let milliseconds_timestamp = duration_since_epoch.as_millis();
+
+    Ok(milliseconds_timestamp)
+}
+
+async fn get_chunk(s3_client: &S3Client, args: &Input) -> Vec<u8> {
+    let byte_range = (args.start_byte, args.end_byte);
+
+    let get_res = s3_client
+        .get_object()
+        .bucket(args.bucket.clone())
+        .key(args.key.clone())
+        .range(format!("bytes={}-{}", byte_range.0, byte_range.1))
+        .send()
+        .await
+        .unwrap();
+
+    let mut buffer: Vec<u8> = Vec::with_capacity(((byte_range.1 - byte_range.0) + 1) as usize);
+    let mut reader = get_res.body.into_async_read();
+    while let Ok(sz) = reader.read_buf(&mut buffer).await {
+        if sz == 0 {
+            break;
+        }
+    }
+    buffer
+}
+
+
+fn hyperparameter_tuning(args: Input,  burst_middleware: Middleware<Bytes>) -> Option<Output> {
+    let burst_middleware = burst_middleware.get_actor_handle();
+    let init_fn = get_timestamp_in_milliseconds().unwrap().to_string();
+
+    let credentials_provider = Credentials::from_keys(
+        args.s3_config.aws_access_key_id.clone(),
+        args.s3_config.aws_secret_access_key.clone(),
+        None
+    );
+
+    let config = match args.s3_config.endpoint.clone() {
+        Some(endpoint) => {
+            aws_sdk_s3::config::Builder::new()
+                .endpoint_url(endpoint)
+                .credentials_provider(credentials_provider)
+                .region(Region::new(args.s3_config.region.clone()))
+                .force_path_style(true)
+                .retry_config(RetryConfig::adaptive())
+                .build()
+        }
+        None => aws_sdk_s3::config::Builder::new()
+            .credentials_provider(credentials_provider)
+            .region(Region::new(args.s3_config.region.clone()))
+            .retry_config(RetryConfig::adaptive())
+            .build(),
+    };
+
+    let s3_client = S3Client::from_conf(config);
+
+    let tokio_runtime = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let buffer = tokio_runtime.block_on(get_chunk(&s3_client, &args));
+
+    let mut messages: Vec<Bytes> = Vec::new();
+    if burst_middleware.info.worker_id != args.base_worker_id {
+        burst_middleware.send(args.base_worker_id, Bytes::from(buffer));
+    } else {
+        messages.push(Bytes::from(buffer));
+        for i in 1..(args.base_worker_id + args.granularity) {
+            messages.push(burst_middleware.recv(i).unwrap());
+        }
+    }
+    let mut buffer: Vec<u8> = Vec::new();
+    for message in messages {
+        buffer.extend_from_slice(&message);
+    }
+
+    let post_download_chunk = get_timestamp_in_milliseconds().unwrap().to_string();
+
+    let mut file = File::create("input.txt").unwrap();
+    file.write_all(&buffer).unwrap();
+
+    let input_gathered = get_timestamp_in_milliseconds().unwrap().to_string();
+
+    if burst_middleware.info.worker_id != args.base_worker_id {
+        burst_middleware.send(args.base_worker_id, Bytes::from("done"));
+    } else {
+        for i in 1..(args.base_worker_id + args.granularity) {
+            burst_middleware.recv(i);
+        }
+    }
+
+    let end_fn = get_timestamp_in_milliseconds().unwrap().to_string();
+
+    Some(Output {
+        success: true,
+        init_fn,
+        post_download_chunk,
+        input_gathered,
+        end_fn,
+    })
+}
+
+pub fn main(args: Value, burst_middleware: Middleware<Bytes>) -> Result<Value, Error> {
+    let input: Input = serde_json::from_value(args)?;
+    println!("Starting hyperparameter tuning: {:?}", input);
+
+    let result = hyperparameter_tuning(input, burst_middleware);
+
+    println!("Done");
+    println!("{:?}", result);
+    serde_json::to_value(result)
+}
---
+++ hypertuning/main.py
@@ -0,0 +1,65 @@
+import argparse
+
+import boto3
+
+from ow_client.openwhisk_executor import OpenwhiskExecutor
+from ow_client.parser import add_burst_to_parser, try_or_except, add_openwhisk_to_parser
+
+
+def add_hypertuning_to_parser(parser):
+    parser.add_argument("--workers", type=int, help="Number of workers to use", required=True)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    add_openwhisk_to_parser(parser)
+    add_hypertuning_to_parser(parser)
+    add_burst_to_parser(parser)
+    args = try_or_except(parser)
+
+    base_payload = {
+        "bucket": "hypertuning",
+        "key": "train.ft.txt.bz2",
+        "s3_config": {
+            "region": "us-east-1",
+            "endpoint": "http://192.168.5.24:9000",
+            "aws_access_key_id": "lab144",
+            "aws_secret_access_key": "astl1a4b4",
+        },
+        "start_byte": None,
+        "end_byte": None,
+        "base_worker_id": None,
+        "granularity": args.granularity,
+    }
+
+    s3_client = boto3.client(
+        's3',
+        region_name=base_payload["s3_config"]["region"],
+        endpoint_url=base_payload["s3_config"]["endpoint"],
+        aws_access_key_id=base_payload["s3_config"]["aws_access_key_id"],
+        aws_secret_access_key=base_payload["s3_config"]["aws_secret_access_key"]
+    )
+
+    input_head = s3_client.head_object(Bucket=base_payload["bucket"], Key=base_payload["key"])
+
+    params = []
+    for i in range(args.workers):
+        payload = base_payload.copy()
+        payload["base_worker_id"] = (i // args.granularity) * args.granularity
+        payload["start_byte"] = (input_head["ContentLength"] // args.workers) * i
+        payload["end_byte"] = (input_head["ContentLength"] // args.workers) * (i + 1)
+        params.append(payload)
+
+    executor = OpenwhiskExecutor(args.ow_host, args.ow_port, args.debug)
+    dt = executor.burst("kmeans-burst",
+                        params,
+                        file="hypertuning/hypertuning.zip",
+                        memory=args.runtime_memory,
+                        burst_size=args.granularity,
+                        debug_mode=args.debug,
+                        backend=args.backend,
+                        chunk_size=args.chunk_size,
+                        custom_image="manriurv/rust_burst:1",
+                        is_zip=True)
+    dt.plot()
+    result = dt.get_results()
+    print("*** HyperTuning finished ***")
